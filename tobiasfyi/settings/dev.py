c0bb9a15987017578d9a620e5c0ebd1e3962619d
tobias-fyi <tobyreaper@gmail.com>
2020-03-05 21:36:26 -0700

    Initial docker image built

--- tobiasfyi/settings/dev.py
+++ tobiasfyi/settings/dev.py
@@ -3,20 +3,17 @@ tobias.fyi :: Development Settings
 """
 
 import os
-from dotenv import load_dotenv
 
 from .base import *
 
-load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"))
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = os.environ.get("DEBUG") == "True"
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
